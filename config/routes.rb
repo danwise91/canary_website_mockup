c950f65b7ee6681303bd93888af2d1ce193b0682
Dan Wise <dwise2@oswego.edu>
2016-05-16 17:24:30 -0400

    added pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get '/how' => 'home#how', as: 'how'
+  get '/uses' => 'home#uses', as: 'uses'
+  get '/why' => 'home#why', as: 'why'
+  get '/plans' => 'home#plans', as: 'plans'
+  get '/help' => 'home#help', as: 'help'
   root 'home#index'
 end
